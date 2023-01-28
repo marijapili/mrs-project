73c0d1e4ec16b3f03dc695ee3a9f27f9fd0b44f5
marija.piliskic <marija.piliskic@gideonbros.ai>
2023-01-28 19:36:22 +0100

    fixes

--- scripts/boid.py
+++ scripts/boid.py
@@ -21,6 +21,7 @@ class Boid():
     def __init__(self, x, circle, max_vel, max_acc, num_of_robots):
 
         rospy.Subscriber('/sphero_{}/odom_est'.format(x), Odometry, self.odom_callback)
+        # rospy.Subscriber('/robot_{}/ground_truth_pose'.format(x), Odometry, self.odom_callback)
         rospy.Subscriber('/obstacles', Float32MultiArray, self.obstacles_callback)
         
 
@@ -36,6 +37,10 @@ class Boid():
         self.circle = circle
         self.max_vel = max_vel
         self.max_acc = max_acc
+
+        self.a = [[0,1,1],[0,0,1],[0,1,0]]
+        self.e = [[0,0],[0.5,0],[1,0]] # linija
+        #self.e = [[0,0],[0.5,0.5],[1,0]] # trokut
 
 
     def get_position(self):
@@ -60,7 +65,7 @@ class Boid():
         for boid in flock:
             dist = np.linalg.norm(boid.get_position() - self.get_position())
             if(self.get_index() != boid.get_index() and dist < self.circle and dist != 0):
-                repellant_vec += (self.get_position() - boid.get_position())/(dist**2)
+                repellant_vec += (self.get_position() - boid.get_position())/(dist**3)
                 j += 1
             
         
@@ -127,36 +132,69 @@ class Boid():
         return acc
 
 
+    def consensus(self, flock):
+
+        acc = np.array([0.0, 0.0])
+        vel_x = 0.0
+        vel_y = 0.0
+
+
+        for i in range(self.num_of_robots):
+            if i != self.get_index():
+                vel_x += self.a[i][self.get_index()]*((flock[i].get_position()[0] - self.get_position()[0]) - (self.e[i][0] - self.e[self.get_index()][0]))
+                vel_y += self.a[i][self.get_index()]*((flock[i].get_position()[1] - self.get_position()[1]) - (self.e[i][1] - self.e[self.get_index()][1]))
+
+        vel = flock[0].get_velocity() +  np.array([vel_x, vel_y])
+
+        if(np.linalg.norm(vel) > 0.0):
+            vel = (vel/np.linalg.norm(vel)) * self.max_vel
+            acc = vel - self.get_velocity()
+        if(np.linalg.norm(acc) > self.max_acc):
+            acc = (acc/np.linalg.norm(acc)) * self.max_acc
+
+        return acc
+
     def apply_rules(self, flock, center_to_target):
         
         # Weight parameters
-        S = 0.4
-        A = 0.5
-        C = 0.6
+        S = 0.5
+        A = 0.15
+        C = 0.5
         T = 0.7
         O = 2
+        K = 0
+
+        
+        acc_target = center_to_target[self.no] - self.get_velocity()
+        if(np.linalg.norm(acc_target) > self.max_acc):
+            acc_target = (acc_target/np.linalg.norm(acc_target)) * self.max_acc
         if(self.no == 0):
-            acc_target = center_to_target[0] - self.get_velocity()
-            if(np.linalg.norm(acc_target) > self.max_acc):
-                acc_target = (acc_target/np.linalg.norm(acc_target)) * self.max_acc
             A = 0
             C = 0
             S = 0
 
-        else:
-            acc_target = 0
+        # else:
+        #     acc_target = 0
 
 
         separation = S*self.separation(flock)
         alignment = A*self.alignment(flock)
         cohesion = C*self.cohesion(flock)
         acc_obst = O*self.obstacle_avoidance()
+        acc_cons = K*self.consensus(flock)
 
-        correction = self.get_velocity() + separation + alignment + cohesion + T*acc_target + acc_obst
+        # print(self.get_velocity(), T*acc_target, acc_cons)
+        
+        correction = self.get_velocity() + separation + alignment + cohesion + T*acc_target + acc_obst + acc_cons
+        # print(correction)
         pub_data = self.calc_publish_data(correction)
 
-        if self.no > 0 and self.calculate_distance(center_to_target[self.no]) < 0.2:
-            pub_data = Twist()
+        print(self.no, self.calculate_distance(center_to_target[self.no]))
+
+        # if self.no > 0 and self.calculate_distance(center_to_target[self.no]) < 0.2:
+        #     pub_data = Twist()
+        # elif self.no == 0 and self.calculate_distance(center_to_target[self.no]) < 0.2:
+        #     pub_data = Twist()
         
         return pub_data
 
@@ -166,8 +204,8 @@ class Boid():
         cmd_vel = Twist()
         R_i = np.linalg.inv(np.array([[cos(phi), -sin(phi)], [sin(phi), cos(phi)]]))
         pub_vel = R_i.dot(vel)
-        cmd_vel.linear.x = 70*pub_vel[0]
-        cmd_vel.linear.y = 70*pub_vel[1]
+        cmd_vel.linear.x = 40*pub_vel[0]
+        cmd_vel.linear.y = 40*pub_vel[1]
         return cmd_vel
 
     def calculate_distance(self,position):
--- scripts/node_01.py
+++ scripts/node_01.py
@@ -17,9 +17,20 @@ class Node():
 
         self.flock = [Boid(x, self.circle, self.max_vel, self.max_acc, self.num_of_robots) for x in range(self.num_of_robots)]
         self.pubs = [rospy.Publisher('/sphero_{}/cmd_vel'.format(x), Twist, queue_size=1) for x in range(self.num_of_robots)]
+        # self.pubs = [rospy.Publisher('/robot_{}/cmd_vel'.format(x), Twist, queue_size=1) for x in range(self.num_of_robots)]
         self.pub_data = []
 
-        self.target = np.array([0.5, 0.5])
+        self.target = [ np.array([1.5, 2.3]), np.array([0.5, 2]), np.array([1.5, 0.5])]
+        self.tr = 0
+
+
+
+    def waypoint_gen(self):
+        if (self.flock[0].calculate_distance(self.flock[0].get_position() - self.target[self.tr])) < 0.1:
+            self.tr += 1
+            if self.tr > len(self.target)-1:
+                self.tr %= len(self.target)
+        return self.target[self.tr]
 
 
 
@@ -33,7 +44,7 @@ class Node():
 
         #avg_center /= j
         #center_to_target  = self.target - avg_center
-        center_to_target  = np.array([self.target-boid.get_position() for boid in self.flock])
+        center_to_target  = np.array([self.waypoint_gen()-boid.get_position() for boid in self.flock])
         dist = np.linalg.norm(center_to_target)
         if(dist > 1.5):
             center_to_target = (center_to_target/dist) * self.max_vel
